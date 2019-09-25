df2cb6d9191a715fe642dfb9a41e301cc9ae527f
Andrew <verannick@gmail.com>
2019-09-25 20:25:35 +0300

    Using @Component and @Inject

---
+++ app/src/main/java/com/example/dagger2/Base.kt
@@ -0,0 +1,6 @@
+package com.example.dagger2
+
+import javax.inject.Inject
+//TODO 4: Create a constructor and annotate it with @Inject
+class Base @Inject constructor() {
+}
--- app/src/main/java/com/example/dagger2/MainActivity.kt
+++ app/src/main/java/com/example/dagger2/MainActivity.kt
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var potion: Potion
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TODO 7: create our component via invoking Dagger[Our Interface].create
+        val potionComponent = DaggerPotionComponent.create()
+        //TODO 8: Call function from component to get an instance of needed class. Call class fun, run the up and see some logs.
+        potion = potionComponent.getPotion()
+        potion.effect()
     }
+//    This example is inspired by Coding in Flow Dagger tutorial on Youtube
+//    in this example we are going to create some potions using dagger so the next step is
+    //TODO 3: Create Potion, base and extra classes
 }
