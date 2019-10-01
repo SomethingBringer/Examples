656539035bc2bf38bd2574424f2caae4d9b30f77
Andrew <verannick@gmail.com>
2019-10-01 19:58:51 +0300

    @Component.Builder implemented

--- app/src/main/java/com/example/dagger2/Potion/CheapBase.kt
+++ app/src/main/java/com/example/dagger2/Potion/CheapBase.kt
@@ -2,13 +2,15 @@ package com.example.dagger2.Potion
 
 import android.util.Log
 import javax.inject.Inject
-
-class CheapBase @Inject constructor(): Base {
+import javax.inject.Named
+//TODO 4: But what if we want to provide 2 objects of the same type (Ignore the @Named annotation for now)
+class CheapBase @Inject constructor(@Named("energy") val energy: Int,
+                                    @Named("stability") val stability :Int): Base {
 
     private val TAG = "Potion"
 
     override fun prepare() {
-        Log.d(TAG, "Cheap base prepared")
+        Log.d(TAG, "Cheap base prepared. Energy: "+ energy+" \n Stability: "+stability)
     }
 
 }
